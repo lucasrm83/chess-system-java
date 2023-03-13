ca142203beaea747ff448024bd8a6e68c6290d5b
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-03-13 09:59:38 -0300

    Implementando Board e Piece

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,6 +1,11 @@
 package program;
 
+import boardgame.Board;
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Board board = new Board(8,8);
+    }
+
 }
