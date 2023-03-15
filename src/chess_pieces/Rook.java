1fdd40c8c15412df55c9a5b868d52476e2af94a2
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-03-14 23:12:57 -0300

    inicio classe rook

---
+++ src/chess_pieces/Rook.java
@@ -0,0 +1,11 @@
+package chess_pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece {
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+}
