9b66c6a6da095828969ff08f865c98d52fbe79d9
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-08-02 15:16:47 -0300

    possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,9 @@ public class ChessMatch {
             throw new ChessException("There is no piece on source position");
 
         }
+        if (!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("There is no possible move for the chosen piece");
+        }
     }
 
 
