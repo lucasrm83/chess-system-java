fd0631b34012720f546d83f776f32a4ad236c95d
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-08-08 02:01:50 -0300

    Promotion improvement

--- src/program/Main.java
+++ src/program/Main.java
@@ -7,10 +7,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -42,7 +39,11 @@ public class Main {
                 }
                 if (chessMatch.getPromoted()!= null){
                     System.out.println("Enter piece for promotion: (B/Q/N/R): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B")&& !type.equals("N") && !type.equals("R")&&!type.equals("Q")){
+                        System.out.println("Invalid value! Enter piece for promotion: (B/Q/N/R): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
 
