d99efd5a2423fb7d439c51ba40224e6b74efe09d
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-08-08 01:50:15 -0300

    special move promotion

--- src/program/Main.java
+++ src/program/Main.java
@@ -40,6 +40,11 @@ public class Main {
                 if(capturedPiece!=null){
                     captured.add(capturedPiece);
                 }
+                if (chessMatch.getPromoted()!= null){
+                    System.out.println("Enter piece for promotion: (B/Q/N/R): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
 
             }
             catch (ChessException e){
